71925b3df337ef1a35c8e3c72e911d748e739376
BhukyaLakshman <lakshmanbhukya1709@gmail.com>
2025-03-09 11:48:15 +0530

    installed router dom and routed to home page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./home";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
@@ -18,3 +20,4 @@ function App() {
 }
 
 export default App;
+
